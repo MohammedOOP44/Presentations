7681f0d344d68a4ff8a6f3a2d12d69dc6ad3aff7
Mohammed <DavidMMooG@gmail.com>
2026-04-02 21:19:07 +0300

    Refactor BankAccount methods for subtraction and addition; improve error handling and maintain consistency

--- sub.method.py
+++ sub.method.py
@@ -8,12 +8,41 @@ class BankAccount:
             return BankAccount(self.balance - other.balance)
         elif isinstance(other,int):
             return BankAccount(self.balance - other)
+        else :
+            return NotImplemented
+        
+        
+    def __mul__(self,other):
+        if isinstance(other,BankAccount):
+            return BankAccount(self.balance * other.balance)
+        elif isinstance(other,int):
+            if other < 0:
+                raise ValueError("No negative numbers allowed")
+            return BankAccount(self.balance * other)
+        else :
+            return NotImplemented
+
     
     def __str__(self):
         return f"Balance: {self.balance}"
     
-
+    def __add__(self,other):
+        if isinstance(other,BankAccount):
+            return BankAccount(self.balance + other.balance)
+        elif isinstance(other,int):
+            if other < 0:
+                raise ValueError("No negative numbers allowed")
+            return BankAccount(self.balance + other)
+        else:
+            return NotImplemented
+        
+print("___Welcome to Bank Account___")
+print("choose the operations ya wanna:")
+print("1.")
+print("2.")
+print("3.")
+    
 acc1 = BankAccount(100)
-acc2 = acc1 - 90
+acc2 = acc1 + 44
 
 print(acc2)
