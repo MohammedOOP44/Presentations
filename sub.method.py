b8a2312dc0167dee9db2bbf977fe3085336c687a
Mohammed <DavidMMooG@gmail.com>
2026-04-02 16:17:21 +0300

    How to conduct a verification

--- sub.method.py
+++ sub.method.py
@@ -1,14 +1,15 @@
+
 class Point:
     def __init__(self,x,y):
         self.x = x 
         self.y = y 
 
-    def __sub__(self,other):
-        new_x = self.x - other.x
-        new_y = self.y - other.y
-        return Point(new_x,new_y)
+    def __sub__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return Point(self.x - other.x, self.y - other.y)
     
 p1 = Point(99,18)
-p2 = Point(17,15)
+p2 = 4 
 p3 = p1 - p2
 print(p3.x,p3.y)
