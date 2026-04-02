a96b134f76e0fb5357a24095fe50b0e5b82dd019
Mohammed <DavidMMooG@gmail.com>
2026-04-02 15:58:40 +0300

    How to use __sub__ methode

---
+++ sub.method.py
@@ -0,0 +1,14 @@
+class Point:
+    def __init__(self,x,y):
+        self.x = x 
+        self.y = y 
+
+    def __sub__(self,other):
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        return Point(new_x,new_y)
+    
+p1 = Point(99,18)
+p2 = Point(17,15)
+p3 = p1 - p2
+print(p3.x,p3.y)
