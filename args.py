a65bd9eb33a59d2291c070ab59c29b72eb943eb1
Mohammed <DavidMMooG@gmail.com>
2026-04-04 20:10:30 +0300

    Refactor my_funtion: revert to original implementation that sums three arguments

--- args.py
+++ args.py
@@ -1,8 +1,6 @@
-def my_funtion(username,**details):
-    print('username',username)
-    print('age',details['age'])
-    print('power',details['power'])
-    print('hobby',details['hobby'])
+def my_funtion(a,b,c):
+    return a+b+c
 
-my_funtion("Ali",age=30,power="hard",hobby="coding")
-        
+nums = [1,2,3]
+result = my_funtion(*nums)
+print(result)
