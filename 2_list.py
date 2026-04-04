996b33d37a139536e9b68f1f215b259f2afbcf47
Mohammed <DavidMMooG@gmail.com>
2026-04-04 14:29:29 +0300

    Refactor matrix operations: improve multiplication method and update display logic

--- 2_list.py
+++ 2_list.py
@@ -1,5 +1,7 @@
 m=[[1, 2, 3],[4, 5, 6]]
-n=[[7, 8],[9, 10], [11, 12]]
+n=[[7, 8],
+   [9, 10], 
+   [11, 12]]
 class Matrix:
     def __init__(self,data):
         self.data = data
@@ -35,8 +37,33 @@ class Matrix:
         else:
             return NotImplemented
         
+    def __mul__(self,other):
+        if isinstance(other,Matrix):
+            result_arr = []
+
+            for i in range(len(self.data)):
+                row = []
+
+                for j in range(len(other.data[0])):
+                    total = 0
+
+                    for k in range(len(other.data)):
+                        total += self.data[i][k] * other.data[k][j]
+
+                    row.append(total)
+
+                result_arr.append(row)
+            return Matrix(result_arr)
+        
+        
+        
+    def __rmul__(self,other):
+        return self.__mul__(other)
+        
 m_list = Matrix(m)
 n_list = Matrix(n)
-(m_list + m_list).display()
+
+(m_list*n_list).display()
+
         
 
