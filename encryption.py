f3d68410ff7445d7a3f14056d0a5104f761c14a8
Mohammed <DavidMMooG@gmail.com>
2026-04-04 21:43:29 +0300

    Refactor encryption.py: separate encryption and decryption logic into distinct lists

--- encryption.py
+++ encryption.py
@@ -1,10 +1,18 @@
 text_user = input("Enter the text: ")
-result = []
+result_enc = []
+result_dec = []
 for char in text_user :
     if char.isalpha() :
         base = ord('a') if char.islower() else ord('A')
-        shifted = (ord(char) - base + 1) % 26 + base 
-        result.append(chr(shifted))
+        shiftedToFront = (ord(char) - base + 1) % 26 + base 
+        shiftedToBehind = (ord(char) - base - 1) % 26 + base
+        result_enc.append(chr(shiftedToFront))
+        result_dec.append(chr(shiftedToBehind))
     else :
-        result.append(char)
-print("".join(result))
+        result_enc.append(char)
+        result_dec.append(char)
+choice = int(input("What did you want: 1.encrypted , 2.decrypted: "))
+if choice == 1:
+    print("".join(result_enc))
+elif choice == 2:
+    print("".join(result_dec))
