9b84e3c084864bc70ecf940265a61c27d9a092cc
Mohammed <DavidMMooG@gmail.com>
2026-04-04 19:55:28 +0300

    Refactor my_funtion: update implementation to accept a username and print user details

--- args.py
+++ args.py
@@ -1,8 +1,8 @@
-def my_funtion(**myval):
-    print('type:', type(myval))
-    print('name:', myval['name'])
-    print('Age:', myval['age'])
-    print('All data:', myval)
+def my_funtion(username,**details):
+    print('username',username)
+    print('age',details['age'])
+    print('power',details['power'])
+    print('hobby',details['hobby'])
 
-my_funtion(name='ali',age=22)
+my_funtion("Ali",age=30,power="hard",hobby="coding")
         
