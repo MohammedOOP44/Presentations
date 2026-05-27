8ec81eba134494dea3f10a7b2ff370fa3c8bd601
Mohammed <DavidMMooG@gmail.com>
2026-05-27 16:49:37 +0300

    Refactor Person, Student, Teacher, and Course classes; implement student addition and course details display

--- Abstraction.py
+++ Abstraction.py
@@ -1,18 +1,75 @@
-from abc import ABC , abstractmethod
-class Paymentprocessor(ABC):
+from abc import ABC, abstractmethod
 
-    @abstractmethod 
-    def process_payment(self,amount):
+class Person(ABC):
+    def __init__(self,name,age):
+        self.name = name 
+        self.age = age
+
+    @abstractmethod
+    def get_role(self):
         pass
 
-    def print_receipt(self,amount):
-        print(f"Receipt generated for: ${amount}")
+    @abstractmethod
+    def show_details(self):
+        pass
 
-class PayPalProcessor(Paymentprocessor):
-    def process_payment(self,amount):
-        print(f"processing ${amount} securely via PayPal API ...")
+class Student(Person):
+    def __init__(self,name,age,student_id,major):
+        super().__init__(name,age)
+        self.student_id = student_id
+        self.major = major 
 
-paypal = PayPalProcessor()
-paypal.process_payment(100)
-paypal.print_receipt(100)
+    def __repr__(self):
+        return f"name: {self.name},age: {self.age},ID: {self.student_id},major: {self.major}"
+
+    def get_role(self):
+        return "STUDENT"
     
+    def show_details(self):
+        return f"name: {self.name},age: {self.age},ID: {self.student_id},major: {self.major}"
+
+class Teacher(Person):
+    def __init__(self,name,age,subject):
+        super().__init__(name,age)
+        self.subject = subject
+
+    def __str__(self):
+        return f"name: {self.name},age: {self.age},subject: {self.subject}"
+    
+    def get_role(self):
+        return "TEACHER"
+    
+    def show_details(self):
+        return f"name: {self.name},age: {self.age},subject: {self.subject}"
+    
+class Course:
+    def __init__(self,course_name,course_code,teacher):
+        self.course_name = course_name
+        self.course_code = course_code
+        self.teacher = teacher
+        self.students = []
+
+    def __str__(self):
+        return f"course: {self.course_name},course_code: {self.course_code},teacher: {self.teacher},students: {self.students}"
+
+    def add_student(self,student):
+        self.students.append(student)
+        
+    def show_course_details(self):
+        return f"course: {self.course_name},course_code: {self.course_code},teacher: {self.teacher},students_enrolled: {self.students}"
+    
+
+teacher1 = Teacher("Adel",55,"scince")
+
+student1 = Student("Mohammed",19,1,"Big Data")
+student2 = Student("Ali",18,2,"Engineering application")
+
+course1 = Course("python",111,teacher1)
+
+course1.add_student(student1)
+course1.add_student(student2)
+
+print(teacher1.show_details())
+print(student1.show_details())
+print(student2.show_details())
+print(course1.show_course_details())
